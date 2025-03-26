bda6fa68199d261236b99d314fe68359b7267850
Minte_DEV <mintesinottamene0917@gmail.com>
2025-03-26 15:16:24 +0300

    Refactors homepage structure and adds new sections
    Replaces the previous features section with a modular approach using dedicated components for Features, Stats, How It Works, Testimonial, FAQs, and a CTA button.
    
    Updates the Next.js configuration to allow images from an external source, enhancing image handling capabilities.
    
    Improves overall readability and maintainability of the homepage code.

---
+++ components/CtaButton.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "next/link";
+import {
+  ArrowRight,
+  Trophy,
+  Target,
+  Sparkles,
+  CheckCircle2,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+export default function CtaButton() {
+  return (
+    <div>
+      <section className="w-full">
+        <div className="mx-auto py-24 gradient rounded-lg">
+          <div className="flex flex-col items-center justify-center space-y-4 text-center max-w-3xl mx-auto">
+            <h2 className="text-3xl font-bold tracking-tighter text-primary-foreground sm:text-4xl md:text-5xl">
+              Ready to Accelerate Your Career?
+            </h2>
+            <p className="mx-auto max-w-[600px] text-primary-foreground/80 md:text-xl">
+              Discover how AI-driven insights can transform your career
+              trajectory and open new opportunities.
+            </p>
+            <Link href="/dashboard" passHref>
+              <Button
+                size="lg"
+                variant="secondary"
+                className="h-11 mt-5 animate-bounce"
+              >
+                Start Your Journey Today <ArrowRight className="ml-2 h-4 w-4" />
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
